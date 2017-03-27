cec261ba30e63cc127d3150ef87af242348552e4
n3k00n3 <3mm4@riseup.net>
2017-03-26 22:17:43 -0300

    starting guess game

--- guess.c
+++ guess.c
@@ -16,5 +16,26 @@ void welcome(){
 
 int main(){
   welcome(); 
+  int number;
+  int secret_number = 42;
+  int correct;
+
+  for(int i = 0; i <= 3; i++){
+    printf("\n\nGive me a number!\n");
+    scanf("%d", &number);
+    printf("You have 3 chances!! [Attempt %d]\n", i+1);
+    correct = number == secret_number;
+    if(correct){
+      printf("Congratulations! The number is %d!!!\n", secret_number);
+      break;
+    }else{
+      if(number > secret_number)
+        printf("Your number is greater than Secret Number!!\n");
+      else
+        printf("Your number is less than Secret Number!!\n");
+      printf("Sorry! You are WRONG!!!\n");
+    }
+  }
+
   return 0;
 }
